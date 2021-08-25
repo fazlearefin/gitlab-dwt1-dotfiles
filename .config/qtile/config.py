d410cb84b497b50e2fcd4bd27bd2a03e9d2018b9
Zoltar358 <dariusz.debicki@post.pl>
2021-08-26 01:27:37 +0200

    Fix for colors table.

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -269,14 +269,16 @@ layouts = [
     layout.Floating(**layout_theme)
 ]
 
-colors = [["#282c34", "#282c34"], # panel background
-          ["#3d3f4b", "#434758"], # background for current screen tab
-          ["#ffffff", "#ffffff"], # font color for group names
-          ["#ff5555", "#ff5555"], # border line color for current tab
-          ["#74438f", "#74438f"], # border line color for 'other tabs' and color for 'odd widgets'
-          ["#4f76c7", "#4f76c7"], # color for the 'even widgets'
-          ["#e1acff", "#e1acff"], # window name
-          ["#ecbbfb", "#ecbbfb"]] # backbround for inactive screens
+colors = [
+    "#282c34",  # panel background
+    "#3d3f4b",  # background for current screen tab
+    "#ffffff",  # font color for group names
+    "#ff5555",  # border line color for current tab
+    "#74438f",  # border line color for 'other tabs' and color for 'odd widgets'
+    "#4f76c7",  # color for the 'even widgets'
+    "#e1acff",  # window name
+    "#ecbbfb"   # backbround for inactive screens
+]
 
 prompt = "{0}@{1}: ".format(os.environ["USER"], socket.gethostname())
 
