1cce34c655285025908e8862466638d029ab9b1c
Derek Taylor <derek@distrotube.com>
2023-08-06 16:51:21 -0500

    Making sure 'switch_group' is set to FALSE for qtile workspace keybindings.

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -159,7 +159,7 @@ for i in groups:
             Key(
                 [mod, "shift"],
                 i.name,
-                lazy.window.togroup(i.name, switch_group=True),
+                lazy.window.togroup(i.name, switch_group=False),
                 desc="Switch to & move focused window to group {}".format(i.name),
             ),
             # Or, use below if you prefer not to switch to that group.
