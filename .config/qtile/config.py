7187a987a84b712d512924eeff935024678b1b51
Derek Taylor <derek@distrotube.com>
2023-08-07 07:23:33 -0500

    Working on a new qtile config.

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -532,11 +532,15 @@ floating_layout = layout.Floating(
     float_rules=[
         # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
-        Match(wm_class="confirmreset"),  # gitk
-        Match(wm_class="makebranch"),  # gitk
-        Match(wm_class="maketag"),  # gitk
-        Match(wm_class="ssh-askpass"),  # ssh-askpass
-        Match(title="branchdialog"),  # gitk
+        Match(wm_class="confirmreset"),   # gitk
+        Match(wm_class="makebranch"),     # gitk
+        Match(wm_class="maketag"),        # gitk
+        Match(wm_class="ssh-askpass"),    # ssh-askpass
+        Match(title="branchdialog"),      # gitk
+        Match(title='Confirmation'),      # tastyworks exit box
+        Match(title='Qalculate!'),        # qalculate-gtk
+        Match(wm_class='kdenlive'),       # kdenlive
+        Match(wm_class='pinentry-gtk-2'), # GPG key password entry
         Match(title="pinentry"),  # GPG key password entry
     ]
 )
