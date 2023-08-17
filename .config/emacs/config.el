d5c30fec325669ef89474dcb2574619d595c9e0b
Derek Taylor <derek@distrotube.com>
2023-08-16 22:52:33 -0500

    Making sure vterm allows for 'C-c' and 'ESC' when trying to exit programs

--- .config/emacs/config.el
+++ .config/emacs/config.el
@@ -457,6 +457,12 @@
 (use-package vterm-toggle
   :after vterm
   :config
+  ;; When running programs in Vterm and in 'insert' mode, make sure that CTRL-c
+  ;; kills the program as it would in any standard terminal/shell.
+  (evil-define-key 'insert vterm-mode-map (kbd "C-c") 'vterm--self-insert)
+  ;; When running programs in Vterm and in 'normal' mode, make sure that ESC
+  ;; kills the program as it would in most standard terminal programs.
+  (evil-define-key 'normal vterm-mode-map (kbd "<escape>") 'vterm--self-insert)
   (setq vterm-toggle-fullscreen-p nil)
   (setq vterm-toggle-scope 'project)
   (add-to-list 'display-buffer-alist
