e93d4662a5bb522457844c34ff7a8b12a96464cb
Derek Taylor <derek@distrotube.com>
2023-08-16 20:47:48 -0500

    Minor edits

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -457,7 +457,7 @@ def init_widgets_screen1():
 # All other monitors' bars will display everything but widgets 22 (systray) and 23 (spacer).
 def init_widgets_screen2():
     widgets_screen2 = init_widgets_list()
-    del widgets_screen2[22:23]
+    del widgets_screen2[22:24]
     return widgets_screen2
 
 # For adding transparency to your bar, add (background="#00000000") to the "Screen" line(s)
