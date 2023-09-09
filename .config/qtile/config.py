999c6d6b252b072aed03d7d3a24c92cc435d32a0
Derek Taylor <derek@distrotube.com>
2023-09-09 08:12:38 -0500

    Adding some floating window rules

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -487,19 +487,28 @@ floating_layout = layout.Floating(
         # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
         Match(wm_class="confirmreset"),   # gitk
+        Match(wm_class="dialog"),         # dialog boxes
+        Match(wm_class="download"),       # downloads
+        Match(wm_class="error"),          # error msgs
+        Match(wm_class="file_progress"),  # file progress boxes
+        Match(wm_class='kdenlive'),       # kdenlive
         Match(wm_class="makebranch"),     # gitk
         Match(wm_class="maketag"),        # gitk
+        Match(wm_class="notification"),   # notifications
+        Match(wm_class='pinentry-gtk-2'), # GPG key password entry
         Match(wm_class="ssh-askpass"),    # ssh-askpass
+        Match(wm_class="toolbar"),        # toolbars
+        Match(wm_class="Yad"),            # yad boxes
         Match(title="branchdialog"),      # gitk
         Match(title='Confirmation'),      # tastyworks exit box
         Match(title='Qalculate!'),        # qalculate-gtk
-        Match(wm_class='kdenlive'),       # kdenlive
-        Match(wm_class='pinentry-gtk-2'), # GPG key password entry
         Match(title="pinentry"),          # GPG key password entry
         Match(title="tastycharts"),       # tastytrade pop-out charts
         Match(title="tastytrade"),        # tastytrade pop-out side gutter
         Match(title="tastytrade - Portfolio Report"), # tastytrade pop-out allocation
-
+        Match(wm_class="tasty.javafx.launcher.LauncherFxApp",
+              title=None
+        ), # tastytrade settings
     ]
 )
 auto_fullscreen = True
